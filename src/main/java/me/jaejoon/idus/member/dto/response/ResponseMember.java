5aec986c4c583731eecdc967baea534ed1d92515
jaejoon <dkansk924@naver.com>
2021-07-04 01:02:34 +0900

    [feat] Response Member dto  추가

---
+++ src/main/java/me/jaejoon/idus/member/dto/response/ResponseMember.java
@@ -0,0 +1,39 @@
+package me.jaejoon.idus.member.dto.response;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.jaejoon.idus.member.domain.Member;
+
+/**
+ * @author dkansk924@naver.com
+ * @since 2021/07/04
+ */
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ResponseMember {
+
+    private String name;
+
+    private String nickname;
+
+    private String tel;
+
+    private String email;
+
+    private String gender;
+
+    private ResponseMember(Member member) {
+        this.name = member.getName();
+        this.nickname = member.getNickName();
+        this.tel = member.getTel();
+        this.email = member.getEmail();
+        this.gender = member.getGender().getName();
+    }
+
+    public static ResponseMember toMapper(Member member) {
+        return new ResponseMember(member);
+    }
+
+}
