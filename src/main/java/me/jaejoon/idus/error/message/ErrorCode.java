c6272b72d27d361168b353227200099b9b411b4e
jaejoon <dkansk924@naver.com>
2021-07-05 04:13:02 +0900

    [feat] 인증 uri 요청시 Token 이 없거나 Bearer 요청이 아닌경우 예외 추가

--- src/main/java/me/jaejoon/idus/error/message/ErrorCode.java
+++ src/main/java/me/jaejoon/idus/error/message/ErrorCode.java
@@ -13,6 +13,9 @@ public enum ErrorCode {
 
     NOT_ALLOW_ARGUMENT(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다."),
 
+    //Auth
+    NOT_EXISTENCE_OR_INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "로그인을 하지않았거나 잘못된 접근입니다."),
+
     //Member
     DUPLICATION_NICKNAME(HttpStatus.CONFLICT, "이미 존재하는 닉네임 입니다."),
     DUPLICATION_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일 입니다.");
