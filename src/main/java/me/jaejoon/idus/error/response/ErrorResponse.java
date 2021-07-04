37a040a6430f5360a4348cb99cb5ca7df8944055
jaejoon <dkansk924@naver.com>
2021-07-04 17:43:18 +0900

    [feat] 공통 ErrorResponse 추가

---
+++ src/main/java/me/jaejoon/idus/error/response/ErrorResponse.java
@@ -0,0 +1,66 @@
+package me.jaejoon.idus.error.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import lombok.Getter;
+import me.jaejoon.idus.error.message.ErrorCode;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+/**
+ * @author dkansk924@naver.com
+ * @since 2021/07/04
+ */
+
+
+@Getter
+@JsonInclude(Include.NON_NULL)
+public class ErrorResponse {
+
+    private final int status;
+    private final String message;
+    private List<ErrorField> errors;
+    private final LocalDateTime timeStamp = LocalDateTime.now();
+
+    public ErrorResponse(ErrorCode errorCode) {
+        this.status = errorCode.getStatus().value();
+        this.message = errorCode.getMessage();
+    }
+
+    public ErrorResponse(ErrorCode errorCode, MethodArgumentNotValidException e) {
+        this.status = errorCode.getStatus().value();
+        this.message = errorCode.getMessage();
+        this.errors = createMessage(e);
+    }
+
+    private List<ErrorField> createMessage(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        return fieldErrors.stream().map(ErrorField::mapper).collect(Collectors.toList());
+    }
+
+
+    @Getter
+    private static class ErrorField {
+
+        private final String field;
+        private final String value;
+        private final String reason;
+
+        private ErrorField(FieldError fieldError) {
+            this.field = fieldError.getField();
+            this.value = Objects.requireNonNull(fieldError.getRejectedValue()).toString();
+            this.reason = fieldError.getDefaultMessage();
+        }
+
+        public static ErrorField mapper(FieldError fieldError) {
+            return new ErrorField(fieldError);
+        }
+    }
+}
+
