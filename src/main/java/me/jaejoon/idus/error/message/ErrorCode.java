2c1cf2da77b64146396cab62e44b829c1a5ea0fa
jaejoon <dkansk924@naver.com>
2021-07-05 22:52:21 +0900

    [feat] 회원 상세 조회(어드민) 추가

--- src/main/java/me/jaejoon/idus/error/message/ErrorCode.java
+++ src/main/java/me/jaejoon/idus/error/message/ErrorCode.java
@@ -15,6 +15,7 @@ public enum ErrorCode {
 
     //Auth
     NOT_EXISTENCE_OR_INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "로그인을 하지않았거나 잘못된 접근입니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "해당 요청에대한 권한이 존재하지 않습니다."),
     LOGIN_FAILED(HttpStatus.BAD_REQUEST, "아이디 또는 비밀번호가 맞지않습니다."),
 
     //Member
