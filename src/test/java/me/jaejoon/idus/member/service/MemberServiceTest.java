8a73d4683dd2d7cbfb6ecce8fc8344c2ead56bd4
jaejoon <dkansk924@naver.com>
2021-07-05 18:04:04 +0900

    [test] login 기능 실패,성공 테스트케이스 추가.

--- src/test/java/me/jaejoon/idus/member/service/MemberServiceTest.java
+++ src/test/java/me/jaejoon/idus/member/service/MemberServiceTest.java
@@ -4,12 +4,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Optional;
+import me.jaejoon.idus.auth.authentication.AuthUser;
+import me.jaejoon.idus.auth.service.JwtService;
 import me.jaejoon.idus.error.message.ErrorCode;
 import me.jaejoon.idus.member.domain.Gender;
 import me.jaejoon.idus.member.domain.Member;
+import me.jaejoon.idus.member.dto.request.RequestMemberLogin;
 import me.jaejoon.idus.member.dto.request.RequestSaveMember;
+import me.jaejoon.idus.member.dto.response.ResponseLoginToken;
 import me.jaejoon.idus.member.exception.DuplicateEmailException;
 import me.jaejoon.idus.member.exception.DuplicateNickname;
+import me.jaejoon.idus.member.exception.LoginFailedException;
 import me.jaejoon.idus.member.repository.MemberRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -31,6 +36,8 @@ class MemberServiceTest {
     @Autowired
     MemberRepository memberRepository;
 
+    @Autowired
+    JwtService jwtService;
 
     @AfterEach
     void clean() {
@@ -142,4 +149,83 @@ class MemberServiceTest {
             .hasMessage(ErrorCode.DUPLICATION_NICKNAME.getMessage());
     }
 
+
+    @Test
+    @DisplayName("회원로그인(성공)")
+    void memberLogin() {
+        //given
+        RequestSaveMember requestSaveMember =
+            new RequestSaveMember(
+                "김재준",
+                "nickname",
+                "@Abcdef0123",
+                "0100000000",
+                "test@email.com",
+                Gender.NONE);
+        memberService.save(requestSaveMember);
+        RequestMemberLogin requestMemberLogin = RequestMemberLogin.builder()
+            .email("test@email.com")
+            .password("@Abcdef0123")
+            .build();
+
+        //when
+        ResponseLoginToken token = memberService.login(requestMemberLogin);
+        AuthUser authUser = jwtService.decode(token.getToken());
+
+        //then
+        assertThat(token).isNotNull();
+        assertThat(authUser.getEmail()).isEqualTo("test@email.com");
+    }
+
+    @Test
+    @DisplayName("회원로그인(실패) - pw 다른경우")
+    void memberLogin_fail() {
+        //given
+        RequestSaveMember requestSaveMember =
+            new RequestSaveMember(
+                "김재준",
+                "nickname",
+                "@Abcdef0123",
+                "0100000000",
+                "test@email.com",
+                Gender.NONE);
+        memberService.save(requestSaveMember);
+        RequestMemberLogin requestMemberLogin = RequestMemberLogin.builder()
+            .email("test@email.com")
+            .password("@bbb1234") //error point
+            .build();
+
+        //when
+        assertThatThrownBy(() -> memberService.login(requestMemberLogin))
+
+            //then
+            .isInstanceOf(LoginFailedException.class)
+            .hasMessage(ErrorCode.LOGIN_FAILED.getMessage());
+    }
+
+    @Test
+    @DisplayName("회원로그인(실패) - ID가 없는경우")
+    void memberLogin_fail2() {
+        //given
+        RequestSaveMember requestSaveMember =
+            new RequestSaveMember(
+                "김재준",
+                "nickname",
+                "@Abcdef0123",
+                "0100000000",
+                "test@email.com",
+                Gender.NONE);
+        memberService.save(requestSaveMember);
+        RequestMemberLogin requestMemberLogin = RequestMemberLogin.builder()
+            .email("kjj@email.com") //error point
+            .password("@Abcdef0123")
+            .build();
+
+        //when
+        assertThatThrownBy(() -> memberService.login(requestMemberLogin))
+
+            //then
+            .isInstanceOf(LoginFailedException.class)
+            .hasMessage(ErrorCode.LOGIN_FAILED.getMessage());
+    }
 }
