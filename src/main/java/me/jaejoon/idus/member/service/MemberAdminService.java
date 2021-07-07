d8c7ae419f276711210874dc732dc47296434f5c
jaejoon <dkansk924@naver.com>
2021-07-07 20:55:37 +0900

    [feat] 회원 검색 서비스,API

--- src/main/java/me/jaejoon/idus/member/service/MemberAdminService.java
+++ src/main/java/me/jaejoon/idus/member/service/MemberAdminService.java
@@ -2,9 +2,13 @@ package me.jaejoon.idus.member.service;
 
 import lombok.RequiredArgsConstructor;
 import me.jaejoon.idus.member.domain.Member;
+import me.jaejoon.idus.member.dto.request.RequestMemberSearch;
 import me.jaejoon.idus.member.dto.response.ResponseMember;
+import me.jaejoon.idus.member.dto.response.ResponseMembersPaging;
 import me.jaejoon.idus.member.exception.NotFoundMemberException;
 import me.jaejoon.idus.member.repository.MemberRepository;
+import me.jaejoon.idus.member.repository.MemberSearchRepository;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,14 +21,23 @@ import org.springframework.stereotype.Service;
 public class MemberAdminService {
 
     private final MemberRepository memberRepository;
+    private final MemberSearchRepository memberSearchRepository;
 
 
     public ResponseMember getMemberInfo(String email) {
         return ResponseMember.toMapper(findByEmailMember(email));
     }
 
+    public ResponseMembersPaging getMemberListIncludingLastOrders(
+        RequestMemberSearch search, Pageable pageable) {
+        return ResponseMembersPaging
+            .toMapper(memberSearchRepository.getMembersIncludingLastOrder(search, pageable));
+    }
+
     private Member findByEmailMember(String email) {
         return memberRepository.findByEmail(email)
             .orElseThrow(NotFoundMemberException::new);
     }
+
+
 }
